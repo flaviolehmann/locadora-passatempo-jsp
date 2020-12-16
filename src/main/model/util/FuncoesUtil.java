64e24a7e18ac0b015597b880e41e8561a51ea9f4
Flávio Lehmann <flaviojunior0899@gmail.com>
2020-12-16 19:25:37 -0300

    rm unused import

--- src/main/model/util/FuncoesUtil.java
+++ src/main/model/util/FuncoesUtil.java
@@ -1,7 +1,6 @@
 package main.model.util;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
